c20836913ce4c60395fc317a86a5afacebe3a23e
Matvey Khort <mdkhort@edu.hse.ru>
2025-04-01 21:29:44 +0300

    baseline.py added

---
+++ GazeTracking/baseline.py
@@ -0,0 +1,37 @@
+import cv2
+import mediapipe as mp
+
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+
+while True:
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+
+        eye_points = landmarks[474:478]
+        for id, landmark in enumerate(eye_points):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
+            if id == 1:
+                x_norm = landmark.x * 2 - 1
+                y_norm = 1 - landmark.y * 2
+
+                coord_text = f"X: {x_norm:.2f}, Y: {y_norm:.2f}"
+                cv2.putText(frame, coord_text, (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    cv2.imshow('Eye Tracking', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
