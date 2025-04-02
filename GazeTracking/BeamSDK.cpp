8008f956a7b9606e193efb2c01ebb1b5254d6a97
Matvey Khort <mdkhort@edu.hse.ru>
2025-04-02 18:11:35 +0300

    BeamSDK.cpp modified, comments added

--- GazeTracking/BeamSDK.cpp
+++ GazeTracking/BeamSDK.cpp
@@ -6,17 +6,21 @@
 #include "eyeware/tracker_client.h"
 
 int main() {
+    // Получаем разрешение монитора
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
     int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
+    // Создаем экземпляр клиента трекинга
     eyeware::TrackerClient tracker;
 
     while (true) {
         if (tracker.connected()) {
+            // Получаем данные положения головы и экрана взгляда
             auto head_pose = tracker.get_head_pose_info();
             auto screen_gaze = tracker.get_screen_gaze_info();
 
             if (!screen_gaze.is_lost) {
+                // Нормализуем координаты взгляда
                 float normalizedX = static_cast<float>(screen_gaze.x) / screenWidth;
                 float normalizedY = static_cast<float>(screen_gaze.y) / screenHeight;
 
@@ -27,6 +31,7 @@ int main() {
 
             std::this_thread::sleep_for(std::chrono::milliseconds(16));
         } else {
+            // Если соединение не установлено, выводим сообщение каждые 2 секунды
             const int MESSAGE_PERIOD_IN_SECONDS = 2;
             std::this_thread::sleep_for(std::chrono::seconds(MESSAGE_PERIOD_IN_SECONDS));
             std::cout << "No connection with tracker server" << std::endl;
